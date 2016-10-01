ba3543f9e8a4709ac7b476b97a159433f27cb414
MKFMIKU <mikumkf@outlook.com>
2016-10-01 12:51:53 +0800

    Add Gaode GPS module

--- app/src/main/java/com/kffuck/mikum/clfgo/MainActivity.java
+++ app/src/main/java/com/kffuck/mikum/clfgo/MainActivity.java
@@ -1,13 +1,49 @@
 package com.kffuck.mikum.clfgo;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AMapLocationClient mLocationClient = null;
+        AMapLocationListener mLocationListener = new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                if (aMapLocation != null) {
+                    if (aMapLocation.getErrorCode() == 0) {
+                        Log.e("Location", String.valueOf(aMapLocation.getLatitude())+" "+String.valueOf(aMapLocation.getLongitude()));
+                    } else {
+                        //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                        Log.e("AmapError", "location Error, ErrCode:"
+                                + aMapLocation.getErrorCode() + ", errInfo:"
+                                + aMapLocation.getErrorInfo());
+                    }
+                }
+            }
+        };
+
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        mLocationClient.setLocationListener(mLocationListener);
+        mLocationClient.startLocation();
     }
+
+
 }
+
